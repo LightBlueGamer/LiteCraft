07f23f29699335b7a2b923dda793d88196f2a7f6
LightBlueGamer <timjonsson03@hotmail.com>
2022-09-26 14:24:10 +0200

    Added join and leave event for members

---
+++ src/events/guildMemberAdd.ts
@@ -0,0 +1,24 @@
+import { EmbedBuilder, GuildMember } from "discord.js"
+
+export default {
+    name: 'guildMemberAdd',
+    once: false,
+    async execute(member: GuildMember) {
+        const channel = member.client.channels.cache.get('970383430971953152')!;
+        let memberCount = member.guild.memberCount.toString();
+
+        if (memberCount.endsWith('1')) memberCount += 'st';
+        else if (memberCount.endsWith('2')) memberCount += 'nd';
+        else if (memberCount.endsWith('3')) memberCount += 'rd';
+        else memberCount += 'th'
+
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Welcome ${member.displayName}!`)
+            .setDescription(`Please read through the rules in <#952980843192811570> and verify yourself in <#952961331600949328>`)
+            .setColor('Random')
+            .setFooter({ text: `You are the ${memberCount} member!` })
+
+        if (channel.isTextBased()) channel.send({ embeds: [embed] });
+    }
+}
